564432d957310e4a6f44cb5e736dbb1ddd7a77da
Bruno Moura <111007233+BrunoMouraDev@users.noreply.github.com>
2023-03-07 08:38:20 -0300

    Add files via upload

---
+++ Atividade3/Pimc/Pimc/Pimc/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace Pimc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblpeso = new System.Windows.Forms.Label();
+            this.txtpeso = new System.Windows.Forms.TextBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.btnlimpar = new System.Windows.Forms.Button();
+            this.btnsair = new System.Windows.Forms.Button();
+            this.lblaltura = new System.Windows.Forms.Label();
+            this.txtaltura = new System.Windows.Forms.TextBox();
+            this.lblText = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblimc = new System.Windows.Forms.Label();
+            this.labelsituação = new System.Windows.Forms.Label();
+            this.txtimc = new System.Windows.Forms.TextBox();
+            this.txtsituacao = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblpeso
+            // 
+            this.lblpeso.AutoSize = true;
+            this.lblpeso.Font = new System.Drawing.Font("Antique Olive CompactPS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpeso.Location = new System.Drawing.Point(46, 103);
+            this.lblpeso.Name = "lblpeso";
+            this.lblpeso.Size = new System.Drawing.Size(49, 14);
+            this.lblpeso.TabIndex = 0;
+            this.lblpeso.Text = "PESO :";
+            // 
+            // txtpeso
+            // 
+            this.txtpeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpeso.Location = new System.Drawing.Point(138, 101);
+            this.txtpeso.Name = "txtpeso";
+            this.txtpeso.Size = new System.Drawing.Size(100, 22);
+            this.txtpeso.TabIndex = 1;
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(35, 340);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(75, 23);
+            this.btncalcular.TabIndex = 3;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // btnlimpar
+            // 
+            this.btnlimpar.Location = new System.Drawing.Point(125, 340);
+            this.btnlimpar.Name = "btnlimpar";
+            this.btnlimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnlimpar.TabIndex = 4;
+            this.btnlimpar.Text = "Limpar";
+            this.btnlimpar.UseVisualStyleBackColor = true;
+            this.btnlimpar.Click += new System.EventHandler(this.btnlimpar_Click);
+            // 
+            // btnsair
+            // 
+            this.btnsair.Font = new System.Drawing.Font("Albertus Medium", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsair.Location = new System.Drawing.Point(216, 340);
+            this.btnsair.Name = "btnsair";
+            this.btnsair.Size = new System.Drawing.Size(75, 23);
+            this.btnsair.TabIndex = 5;
+            this.btnsair.Text = "Fechar";
+            this.btnsair.UseVisualStyleBackColor = true;
+            this.btnsair.Click += new System.EventHandler(this.btnsair_Click);
+            // 
+            // lblaltura
+            // 
+            this.lblaltura.AutoSize = true;
+            this.lblaltura.Font = new System.Drawing.Font("Antique Olive CompactPS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblaltura.Location = new System.Drawing.Point(47, 149);
+            this.lblaltura.Name = "lblaltura";
+            this.lblaltura.Size = new System.Drawing.Size(66, 14);
+            this.lblaltura.TabIndex = 5;
+            this.lblaltura.Text = "ALTURA :";
+            // 
+            // txtaltura
+            // 
+            this.txtaltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtaltura.Location = new System.Drawing.Point(138, 146);
+            this.txtaltura.Name = "txtaltura";
+            this.txtaltura.Size = new System.Drawing.Size(100, 22);
+            this.txtaltura.TabIndex = 2;
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Font = new System.Drawing.Font("Antique Olive CompactPS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText.Location = new System.Drawing.Point(46, 57);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(212, 20);
+            this.lblText.TabIndex = 7;
+            this.lblText.Text = "CALCULADORA DE IMC";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Location = new System.Drawing.Point(53, 212);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(205, 98);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblimc
+            // 
+            this.lblimc.AutoSize = true;
+            this.lblimc.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblimc.Font = new System.Drawing.Font("Antique Olive CompactPS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblimc.Location = new System.Drawing.Point(61, 238);
+            this.lblimc.Name = "lblimc";
+            this.lblimc.Size = new System.Drawing.Size(56, 16);
+            this.lblimc.TabIndex = 9;
+            this.lblimc.Text = "I.M.C :";
+            // 
+            // labelsituação
+            // 
+            this.labelsituação.AutoSize = true;
+            this.labelsituação.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.labelsituação.Font = new System.Drawing.Font("Antique Olive CompactPS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelsituação.Location = new System.Drawing.Point(61, 271);
+            this.labelsituação.Name = "labelsituação";
+            this.labelsituação.Size = new System.Drawing.Size(84, 15);
+            this.labelsituação.TabIndex = 10;
+            this.labelsituação.Text = "SITUAÇÃO :";
+            // 
+            // txtimc
+            // 
+            this.txtimc.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtimc.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtimc.Enabled = false;
+            this.txtimc.Font = new System.Drawing.Font("Antique Olive CompactPS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtimc.Location = new System.Drawing.Point(138, 239);
+            this.txtimc.Name = "txtimc";
+            this.txtimc.Size = new System.Drawing.Size(100, 14);
+            this.txtimc.TabIndex = 11;
+            this.txtimc.TextChanged += new System.EventHandler(this.txtimc_TextChanged);
+            // 
+            // txtsituacao
+            // 
+            this.txtsituacao.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtsituacao.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtsituacao.Enabled = false;
+            this.txtsituacao.Font = new System.Drawing.Font("Antique Olive CompactPS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtsituacao.Location = new System.Drawing.Point(158, 271);
+            this.txtsituacao.Name = "txtsituacao";
+            this.txtsituacao.Size = new System.Drawing.Size(80, 14);
+            this.txtsituacao.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(332, 380);
+            this.Controls.Add(this.txtsituacao);
+            this.Controls.Add(this.txtimc);
+            this.Controls.Add(this.labelsituação);
+            this.Controls.Add(this.lblimc);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.txtaltura);
+            this.Controls.Add(this.lblaltura);
+            this.Controls.Add(this.btnsair);
+            this.Controls.Add(this.btnlimpar);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.txtpeso);
+            this.Controls.Add(this.lblpeso);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CALCULADORA DE IMC";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblpeso;
+        private System.Windows.Forms.TextBox txtpeso;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Button btnlimpar;
+        private System.Windows.Forms.Button btnsair;
+        private System.Windows.Forms.Label lblaltura;
+        private System.Windows.Forms.TextBox txtaltura;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblimc;
+        private System.Windows.Forms.Label labelsituação;
+        private System.Windows.Forms.TextBox txtimc;
+        private System.Windows.Forms.TextBox txtsituacao;
+    }
+}
+
---
+++ Atividade3/Pimc/Pimc/Pimc/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pimc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnlimpar_Click(object sender, EventArgs e)
+        {
+            txtaltura.Clear();
+            txtimc.Clear();
+            txtpeso.Clear();
+            txtsituacao.Clear();
+        }
+
+        private void txtimc_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        Double Peso, altura,imc;
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            if (Double.TryParse(txtpeso.Text, out Peso) && Double.TryParse(txtaltura.Text, out altura))
+            {
+                if ((altura <= 0) || (Peso <= 0))
+                    MessageBox.Show("Valores devem ser maior que Zero");
+                else
+                {
+                    imc = Peso / (altura * altura);
+                    imc = Math.Round(imc, 1); //Sistema que Arredonda
+
+                    txtimc.Text = imc.ToString("N1");
+
+                    if (imc < 18.5)
+                        txtsituacao.Text = "Magreza";
+                    else if (imc <= 24.9)
+                        txtsituacao.Text = "Normal";
+                    else if (imc <= 29.9)
+                        txtsituacao.Text = "SobrePeso";
+                    else if (imc <= 39.9)
+                        txtsituacao.Text = "Obesidade";
+                    else
+                        txtsituacao.Text = "Obesidade Grave";
+
+                }
+            }
+            else
+                MessageBox.Show("Valores Ivalidos");
+        }
+    }
+}
