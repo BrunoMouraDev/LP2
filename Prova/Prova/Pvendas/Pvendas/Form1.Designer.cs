10ca70b4eb4a25d7d5a743d996510dcb783f05e5
Bruno Moura <111007233+BrunoMouraDev@users.noreply.github.com>
2023-03-07 08:57:03 -0300

    Add files via upload

---
+++ Prova/Prova/Pvendas/Pvendas/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Pvendas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstbxValores = new System.Windows.Forms.ListBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstbxValores
+            // 
+            this.lstbxValores.FormattingEnabled = true;
+            this.lstbxValores.Location = new System.Drawing.Point(196, 64);
+            this.lstbxValores.Name = "lstbxValores";
+            this.lstbxValores.Size = new System.Drawing.Size(382, 355);
+            this.lstbxValores.TabIndex = 1;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(40, 200);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(127, 62);
+            this.btnVerificar.TabIndex = 2;
+            this.btnVerificar.Text = "VERIFICAR";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 498);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.lstbxValores);
+            this.Name = "Form1";
+            this.Text = "Pvendas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstbxValores;
+        private System.Windows.Forms.Button btnVerificar;
+    }
+}
+
---
+++ Prova/Prova/Pvendas/Pvendas/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+namespace Pvendas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+
+            int N = 0;
+            int M = 0;
+            int Totalmes = 0;
+            double Totalgeral = 0;
+
+
+
+            var auxiliar2 = Interaction.InputBox("Total de Meses: ", "Número de Meses");
+            int.TryParse(auxiliar2, out N);
+
+            int[,] matrizVendas = new int[N, 4];
+            for (int j = 0; j < N; j++)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    var auxiliar = Interaction.InputBox("Valor de Vendas: ", "Entrada de Valor de Vendas");
+
+                    int.TryParse(auxiliar, out matrizVendas[j, i]);
+                    Totalmes += matrizVendas[j, i];
+                    lstbxValores.Items.Add("Total do Mes: " + (M + 1) + " Semana: " + (i + 1) + " " + matrizVendas[j, i]);
+
+                }
+                lstbxValores.Items.Add(">>Total do Mes: " + Totalmes.ToString("N2"));
+                lstbxValores.Items.Add("\n -------------------------------");
+                Totalmes = 0;
+                M += 1;
+            }
+
+            for (int j = 0; j < N; j++)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    Totalgeral += matrizVendas[j, i];
+                }
+            }
+
+            lstbxValores.Items.Add(">>Total Geral: " + Totalgeral.ToString("N2"));
+
+        }
+
+ 
+    }
+}
